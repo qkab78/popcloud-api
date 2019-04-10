fc773655a608691e72835887909b09f74bbe0da0
Academie-GMC <service.informatique@academie-gmc.com>
2019-04-10 17:01:18 +0200

    AWS functions implemented

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-const Koa = require('koa');
+const Koa = require("koa");
 const Router = require("koa-router");
-const config = require('./config');
+const config = require("./config");
 const app = new Koa();
-const router = new Router()
-require('./services/koa-service').start(app, router, config.PORT);
+const router = new Router();
+require("./services/koa-service").start(app, router, config.PORT);
